354c1ecdf0aa32c624e76acf2afb5650a60332a4
Joff Tiquez <jofftiquez@gmail.com>
2018-03-24 13:29:43 +0800

    Updated tester file

--- tester.js
+++ tester.js
@@ -1,16 +1,50 @@
 const { Selector } = require('./dist');
 
-console.log(Selector)
+const selector = new Selector('output', 'styles');
 
-const btn = new Selector('class', 'btn');
-const btnPrimary = new Selector('class', 'btn-primary');
+selector
+  .create('tag', 'h1')
+  .addProps('color', 'green')
+  .addProps('font-weight', 'bold')
+  .build();
 
-btn
+selector
+  .create('class', 'container')
+  .addProps('padding', '20px')
+  .addProps('background', 'lightgrey')
+  .build();
+
+selector
+  .create('class', 'btn')
   .addProps('width', '100px')
   .addProps('height', '40px')
   .build();
 
-btnPrimary
+selector
+  .create('class:hover', 'btn')
+  .addProps('cursor', 'pointer')
+  .build();
+
+selector
+  .create('class', 'btn-primary')
   .addProps('color', 'white')
   .addProps('background', 'blue')
+  .build();
+
+selector
+  .create('class:hover', 'btn-primary')
+  .addProps('width', '110px')
+  .addProps('height', '45px')
+  .build();
+
+selector
+  .create('class', 'btn-secondary')
+  .addProps('color', 'white')
+  .addProps('background', 'green')
+  .build();
+
+selector
+  .create('class', 'btn-accent')
+  .addProps('color', 'black')
+  .addProps('background', 'rgba(245, 253, 233, 0.5)')
   .build();
