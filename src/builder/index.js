8ba9eb69ef485a520c3fe05c308b0dbd55293b02
Joff Tiquez <jofftiquez@gmail.com>
2018-03-24 13:30:25 +0800

    Removed unlink function

--- src/builder/index.js
+++ src/builder/index.js
@@ -1,10 +1,14 @@
 import fs from 'fs'; 
 
-export const builder = async (selector, filename) => {
-  fs.unlink(`${ filename }.css`, (err) => {
-    const writer = fs.createWriteStream(`${ filename }.css`, {
+export const builder = async (selector, fileName) => {
+  // fs.unlink(`${ fileName }.css`, (err) => {
+  //   if(err) {
+  //     console.error(err);
+  //     return;
+  //   }
+    const writer = fs.createWriteStream(`${ fileName }.css`, {
       flags: 'a'
     });
     writer.write(`${ selector }\n`);
-  });
+  // });
 } 
