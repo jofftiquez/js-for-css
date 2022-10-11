d2bd4f575ea5d9a995ef8b510101161c80a3c29f
chaaals <charlesching0808@gmail.com>
2022-10-12 02:30:15 +0800

    feat: new types (pseudo elements) and implemented css groupings

--- src/selector/index.js
+++ src/selector/index.js
@@ -1,47 +1,50 @@
-import { builder } from '../builder';
+import { builder } from "../builder";
 
-function extractProps () {
+function extractProps() {
   var props = ``;
-  this.selector.props.forEach(prop => {
+  this.selector.props.forEach((prop) => {
     for (let key in prop) {
-      props += `${ key }: ${ prop[key] }; `;
+      props += `${key}: ${prop[key]}; `;
     }
   });
   return props;
 }
-
 export class Selector {
   constructor(fileName) {
-    this.fileName = 'styles';
+    this.fileName = "styles";
     this.selector = {
-      type: '',
-      name: '',
-      props: []
+      type: "",
+      name: "",
+      props: [],
     };
   }
 
   create(type, name) {
-    if(!type) {
-      console.error(`js-for-css error: A selector requires a selector type. 'type' is ${ typeof type }. create(type, name)`);
+    if (!type) {
+      console.error(
+        `js-for-css error: A selector requires a selector type. 'type' is ${typeof type}. create(type, name)`
+      );
       return;
     }
-    
-    if(!name) {
-      console.error(`js-for-css error: A selector requires a selector name. 'name' is ${ typeof name }. create(type, name)`);
+
+    if (!name) {
+      console.error(
+        `js-for-css error: A selector requires a selector name. 'name' is ${typeof name}. create(type, name)`
+      );
       return;
     }
 
     this.selector = {
       type: type,
-      name: name,
-      props: []
+      name: name.split(",").join("").split(" "),
+      props: [],
     };
 
     return this;
   }
 
   addProps(name, value) {
-    if(typeof name === 'object' && !value) {
+    if (typeof name === "object" && !value) {
       // object
     }
 
@@ -51,37 +54,105 @@ export class Selector {
     return this;
   }
 
+  buildSelectorString(type, name, suffix) {
+    switch (type) {
+      case "tag":
+        if (name.length === 1) {
+          const [slctr] = name;
+          return suffix ? `${slctr}${suffix}` : slctr;
+        }
+
+        return name
+          .map((slctr) => (suffix ? `.${slctr}${suffix}` : `${slctr}`))
+          .join(",");
+
+      case "class":
+        if (name.length === 1) {
+          const [slctr] = name;
+          return suffix ? `.${slctr}${suffix}` : `.${slctr}`;
+        }
+
+        return name
+          .map((slctr) => (suffix ? `.${slctr}${suffix}` : `.${slctr}`))
+          .join(",");
+
+      case "id":
+        if (name.length === 1) {
+          const [slctr] = name;
+          return suffix ? `#${slctr}${suffix}` : `#${slctr}`;
+        }
+
+        return name
+          .map((slctr) => (suffix ? `#${slctr}${suffix}` : `#${slctr}`))
+          .join(",");
+
+      default:
+        break;
+    }
+  }
+
   build(options) {
-    let selector = '';
+    let selector = "";
     const { type, name, props } = this.selector;
 
-    switch(type) {
-      case 'class': 
-        selector = `.${ name }`; break;
-      case 'class:active':
-        selector = `.${ name }:active`; break;
-      case 'class:focus':
-        selector = `.${ name }:focus`; break;
-      case 'class:hover':
-        selector = `.${ name }:hover`; break;
-      case 'tag': 
-        selector = `${ name }`; break;
-      case 'tag:active':
-        selector = `${ name }:active`; break;
-      case 'tag:focus':
-        selector = `${ name }:focus`; break;
-      case 'tag:hover':
-        selector = `${ name }:hover`; break;
-      case 'id': 
-        selector = `#${ name }`; break;
-      case 'id:active':
-        selector = `#${ name }:active`; break;
-      case 'id:focus':
-        selector = `#${ name }:focus`; break;
-      case 'id:hover':
-        selector = `#${ name }:hover`; break;
-      default:;break;
+    switch (type) {
+      case "class":
+        selector = this.buildSelectorString("class", name);
+        break;
+      case "class:active":
+        selector = this.buildSelectorString("class", name, ":active");
+        break;
+      case "class:focus":
+        selector = this.buildSelectorString("class", name, ":focus");
+        break;
+      case "class:hover":
+        selector = this.buildSelectorString("class", name, ":hover");
+        break;
+      case "class::before":
+        selector = this.buildSelectorString("class", name, "::before");
+        break;
+      case "class::after":
+        selector = this.buildSelectorString("class", name, "::after");
+        break;
+      case "tag":
+        selector = this.buildSelectorString("tag", name);
+        break;
+      case "tag:active":
+        selector = this.buildSelectorString("tag", name, ":active");
+        break;
+      case "tag:focus":
+        selector = this.buildSelectorString("tag", name, ":focus");
+        break;
+      case "tag:hover":
+        selector = this.buildSelectorString("tag", name, ":hover");
+        break;
+      case "tag::before":
+        selector = this.buildSelectorString("tag", name, "::before");
+        break;
+      case "tag::after":
+        selector = this.buildSelectorString("tag", name, "::after");
+        break;
+      case "id":
+        selector = this.buildSelectorString("id", name);
+        break;
+      case "id:active":
+        selector = this.buildSelectorString("id", name, ":active");
+        break;
+      case "id:focus":
+        selector = this.buildSelectorString("id", name, ":focus");
+        break;
+      case "id:hover":
+        selector = this.buildSelectorString("id", name, ":hover");
+        break;
+      case "id::before":
+        selector = this.buildSelectorString("id", name, "::before");
+        break;
+      case "id::after":
+        selector = this.buildSelectorString("id", name, "::after");
+        break;
+      default:
+        break;
     }
-    builder(`${ selector } { ${ extractProps.bind(this)() } }`, this.fileName);
+    builder(`${selector} { ${extractProps.bind(this)()} }`, this.fileName);
   }
 }
--- tester.js
+++ tester.js
@@ -1,70 +1,89 @@
-const { Selector } = require('./dist');
+const { Selector } = require("./dist");
 
-const selector = new Selector('styles');
+const selector = new Selector("styles");
 
 selector
-  .create('tag', 'h1')
-  .addProps('color', 'green')
-  .addProps('font-weight', 'bold')
+  .create("tag", "h1")
+  .addProps("color", "green")
+  .addProps("font-weight", "bold")
   .build();
 
 selector
-  .create('class', 'container')
-  .addProps('padding', '20px')
-  .addProps('background', 'lightgrey')
+  .create("class", "container")
+  .addProps("padding", "20px")
+  .addProps("background", "lightgrey")
   .build();
 
 selector
-  .create('class', 'btn')
-  .addProps('width', '100px')
-  .addProps('height', '40px')
+  .create("class", "btn")
+  .addProps("width", "100px")
+  .addProps("height", "40px")
+  .build();
+
+selector.create("class:hover", "btn").addProps("cursor", "pointer").build();
+
+selector
+  .create("class", "btn-primary")
+  .addProps("color", "white")
+  .addProps("background", "blue")
   .build();
 
 selector
-  .create('class:hover', 'btn')
-  .addProps('cursor', 'pointer')
+  .create("class:hover", "btn-primary")
+  .addProps("width", "110px")
+  .addProps("height", "45px")
   .build();
 
 selector
-  .create('class', 'btn-primary')
-  .addProps('color', 'white')
-  .addProps('background', 'blue')
+  .create("class", "btn-secondary")
+  .addProps("color", "white")
+  .addProps("background", "green")
   .build();
 
 selector
-  .create('class:hover', 'btn-primary')
-  .addProps('width', '110px')
-  .addProps('height', '45px')
-  .build();
-
-selector
-  .create('class', 'btn-secondary')
-  .addProps('color', 'white')
-  .addProps('background', 'green')
-  .build();
-
-selector
-  .create('class', 'btn-accent')
-  .addProps('color', 'black')
-  .addProps('background', 'rgba(245, 253, 233, 0.5)')
+  .create("class", "btn-accent")
+  .addProps("color", "black")
+  .addProps("background", "rgba(245, 253, 233, 0.5)")
   .build();
 
 // id
 
 selector
-  .create('id', 'foo')
-  .addProps('color', 'green')
-  .addProps('font-weight', 'bold')
+  .create("id", "foo")
+  .addProps("color", "green")
+  .addProps("font-weight", "bold")
+  .build();
+
+selector.create("id:hover", "foo").build();
+
+selector.create("id:focus", "foo").build();
+
+selector.create("id:active", "foo").build();
+
+// css groupings
+
+selector
+  .create("tag", "h2, h3, h4")
+  .addProps("position", "relative")
+  .addProps("color", "green")
+  .addProps("font-weight", "bold")
   .build();
 
 selector
-  .create('id:hover', 'foo')
+  .create("tag::after", "h2, h3, h4")
+  .addProps("content", "''")
+  .addProps("position", "absolute")
+  .addProps("top", 0)
+  .addProps("right", 0)
+  .addProps("width", "40px")
+  .addProps("aspect-ratio", "1")
+  .addProps("background-color", "black")
   .build();
 
-selector
-  .create('id:focus', 'foo')
-  .build();
-  
-selector
-  .create('id:active', 'foo')
-  .build();
+selector.create("class", "class1, class2, class3").build();
+
+selector.create("class:hover", "class1, class2, class3").build();
+
+selector.create("id", "id1, id2, id3").build();
+
+selector.create("id::before", "id1, id2, id3").build();
